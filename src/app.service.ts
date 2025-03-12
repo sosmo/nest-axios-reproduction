533493ca2025ccea41154713dcb7727970ca28cc
Sampo Osmonen <sampo.osmonen@gmail.com>
2025-03-12 12:40:47 +0200

    Issue reproduction

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+export class AppService implements OnApplicationBootstrap {
+  constructor(private readonly httpService: HttpService) {}
+
+  async onApplicationBootstrap() {
+    console.log('\nSTART app.service');
+
+    try {
+      await firstValueFrom(this.httpService.get('https://example.sfdsdf'));
+    } catch (e) {
+      console.log('\nStack from http service:\n');
+      console.log(e.stack);
+    }
+
+    try {
+      await this.httpService.axiosRef.get('https://example.sfdsdf')
+    } catch (e) {
+      console.log('\nStack from raw axios client:\n');
+      console.log(e.stack);
+    }
   }
 }
